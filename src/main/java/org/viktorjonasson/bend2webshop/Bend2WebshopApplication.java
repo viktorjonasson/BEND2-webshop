a81b0a9fca988f0d472a6730c4049c2170aca130
viktorjonasson <viktorjonasson@gmail.com>
2025-08-21 10:19:55 +0200

    Initial commit with structure and dependencies

---
+++ src/main/java/org/viktorjonasson/bend2webshop/Bend2WebshopApplication.java
@@ -0,0 +1,13 @@
+package org.viktorjonasson.bend2webshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bend2WebshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bend2WebshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/viktorjonasson/bend2webshop/Bend2WebshopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.viktorjonasson.bend2webshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bend2WebshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
