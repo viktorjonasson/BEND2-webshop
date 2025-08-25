3b8912b7778a9264a325dd9a092fe065618743ab
anton <antonlarsson.musik@gmail.com>
2025-08-25 14:07:48 +0200

    added missing files

---
+++ src/main/java/org/example/bend2gradle/Bend2GradleApplication.java
@@ -0,0 +1,13 @@
+package org.example.bend2gradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bend2GradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bend2GradleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bend2gradle/Bend2GradleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bend2gradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bend2GradleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
