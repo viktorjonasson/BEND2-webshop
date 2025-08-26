15752c3dbd5541bf8ceab4f268e761b7e8277199
Andy Tran <andy.tran@hotmail.se>
2025-08-26 17:21:18 +0200

    add controller for view all order

--- src/main/java/org/example/BEND2webshop/controllers/PurchasesController.java
+++ src/main/java/org/example/BEND2webshop/controllers/PurchasesController.java
@@ -1,4 +1,26 @@
 package org.example.BEND2webshop.controllers;
 
+import org.springframework.ui.Model;
+import org.example.BEND2webshop.services.PurchaseService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping
+
 public class PurchasesController {
+
+    private final PurchaseService purchaseService;
+
+    public PurchasesController(PurchaseService purchaseService) {
+        this.purchaseService = purchaseService;
+    }
+
+    @GetMapping
+    public String getPurchases(Model model) {
+        model.addAttribute("purchases", purchaseService.getAllPurchases());
+        return "purchases";
+    }
 }
