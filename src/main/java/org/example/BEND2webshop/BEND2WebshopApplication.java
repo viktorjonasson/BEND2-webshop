293138e96e22847b3f6896203cce62b65990a15e
anton <antonlarsson.musik@gmail.com>
2025-08-26 13:48:09 +0200

    created User and UserRole and bootstrapped DB with two users with role 'admin' and 'customer'.

--- src/main/java/org/example/BEND2webshop/BEND2WebshopApplication.java
+++ src/main/java/org/example/BEND2webshop/BEND2WebshopApplication.java
@@ -1,13 +1,27 @@
 package org.example.BEND2webshop;
 
+import org.example.BEND2webshop.services.UserDataSeeder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BEND2WebshopApplication {
 
+    @Autowired
+    private UserDataSeeder userDataSeeder;
+
     public static void main(String[] args) {
+
         SpringApplication.run(BEND2WebshopApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner() {
+        return args -> {
+            userDataSeeder.Seed();
+        };
+    }
 }
---
+++ src/main/java/org/example/BEND2webshop/configs/WebSecurityConfig.java
@@ -0,0 +1,54 @@
+package org.example.BEND2webshop.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/home").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        List<UserDetails> users = List.of(
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build(),
+                User.withDefaultPasswordEncoder()
+                        .username("bingo")
+                        .password("password")
+                        .roles("ADMIN")
+                        .build()
+        );
+
+        users.forEach(u -> System.out.println(u.getAuthorities()));
+
+        return new InMemoryUserDetailsManager(users);
+    }
+}
