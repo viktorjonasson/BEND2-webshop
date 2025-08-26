9af0c2ed9574a9fe67b20aa29de83f705c51713b
anton <antonlarsson.musik@gmail.com>
2025-08-26 16:20:02 +0200

    users can now log in and sign out with roles admin/user and passwords are encrypted in db

---
+++ src/main/java/org/example/BEND2webshop/services/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package org.example.BEND2webshop.services;
+
+
+import org.example.BEND2webshop.models.User;
+import org.example.BEND2webshop.repositories.UserRepository;
+import org.example.BEND2webshop.security.ConcreteUserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userRepository.findUserByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Could not find user");
+        }
+
+        return new ConcreteUserDetails(user);
+    }
+
+}
