a30b6aec97a9b0055b284b470adef1df2fc79794
anton <antonlarsson.musik@gmail.com>
2025-09-29 15:28:09 +0200

    edited application.properties and added test profile and wrote ci pipeline for testing

---
+++ src/test/java/org/example/BEND2webshop/services/UserServiceTest.java
@@ -0,0 +1,17 @@
+package org.example.BEND2webshop.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class UserServiceTest {
+
+    @Test
+    void saveUser() {
+//        fail();
+    }
+}
