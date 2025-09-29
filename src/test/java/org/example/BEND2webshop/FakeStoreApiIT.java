5b44b208faa821f469364776ec1f5dc4d7ed0c68
anton <antonlarsson.musik@gmail.com>
2025-09-29 16:02:57 +0200

    minor change

--- src/test/java/org/example/BEND2webshop/FakeStoreApiIT.java
+++ src/test/java/org/example/BEND2webshop/FakeStoreApiIT.java
@@ -3,12 +3,14 @@ package org.example.BEND2webshop;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles("test")
+@SpringBootTest
 public class FakeStoreApiIT {
 
     @Test
