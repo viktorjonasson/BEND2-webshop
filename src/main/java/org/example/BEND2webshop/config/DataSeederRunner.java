ebe49f58b4cd436ca4a0adbebdc21d6ada5e2396
viktorjonasson <viktorjonasson@gmail.com>
2025-10-01 09:33:24 +0200

    Adderade i DataSeederRunner att inte köra vid tester med profile "test". I test.properties också adderat att SQL aldrig initieras samt att den bara kör tester med profile "test"

--- src/main/java/org/example/BEND2webshop/config/DataSeederRunner.java
+++ src/main/java/org/example/BEND2webshop/config/DataSeederRunner.java
@@ -2,9 +2,11 @@ package org.example.BEND2webshop.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("!test")
 public class DataSeederRunner implements CommandLineRunner {
 
     @Autowired
